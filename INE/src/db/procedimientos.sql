99daebe8a1d8c741d1ac097cc35ebb36c01dd239
DeusArcana <DeusArcana@outlook.com>
2018-06-29 01:11:29 -0600

    SP busquedaInventario
    Corrección en el control de flujo de busquedaInventario.

--- INE/src/db/procedimientos.sql
+++ INE/src/db/procedimientos.sql
@@ -163,47 +163,96 @@ BEGIN
 
 	IF (`Inv_Filtro` < 11) THEN
 		BEGIN
-			SELECT 	CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`),
-					`Inventario`.`Nombre_Prod`,
-					`Inventario`.`Descripcion`,  
-					`Inventario`.`Ubicacion`,
-					`Inventario`.`Marca`,
-					`Inventario`.`Observaciones`,
-					`Inventario`.`No_Serie`,
-					`Inventario`.`Modelo`, 
-					`Inventario`.`Color`, 	
-					`Inventario`.`Fecha_Compra`, 
-					`Inventario`.`Factura`, 
-					`Inventario`.`Importe`
-				FROM `INE`.`Inventario`
-					WHERE  
-						CASE -- Filtro 1
-							WHEN `Inv_Estatus` IS NOT NULL THEN `Inventario`.`Estatus`= `Inv_Estatus`
-							WHEN `Inv_Estatus` IS NULL THEN 1
-						END
-					AND 
-						CASE -- Filtro 2
-							WHEN `Inv_Folio` IS NOT NULL THEN `Inventario`.`Folio` = `Inv_Folio` 
-							WHEN `Inv_Folio` IS NULL THEN 1
-						END
-					AND
-						CASE -- Filtro 3
-							WHEN `Inv_Busqueda` IS NULL THEN 1
-							WHEN `Inv_Filtro` = 0	THEN CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`) LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 1	THEN `Inventario`.`Nombre_Prod`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 2	THEN `Inventario`.`Descripcion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 3	THEN `Inventario`.`Ubicacion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 4	THEN `Inventario`.`Marca`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 5	THEN `Inventario`.`Observaciones`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 6	THEN `Inventario`.`No_Serie`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 7	THEN `Inventario`.`Modelo`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 8	THEN `Inventario`.`Color`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 9	THEN `Inventario`.`Fecha_Compra`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
-							WHEN `Inv_Filtro` = 10	THEN `Inventario`.`Factura`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
-						END;
+			IF NOT (`Inv_Estatus` = 'Asignado') THEN
+				BEGIN
+					SELECT 	CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`),
+									`Inventario`.`Nombre_Prod`,
+									`Inventario`.`Descripcion`,  
+									`Inventario`.`Ubicacion`,
+									`Inventario`.`Marca`,
+									`Inventario`.`Observaciones`,
+									`Inventario`.`No_Serie`,
+									`Inventario`.`Modelo`, 
+									`Inventario`.`Color`, 	
+									`Inventario`.`Fecha_Compra`, 
+									`Inventario`.`Factura`, 	
+									`Inventario`.`Importe`
+								FROM `INE`.`Inventario`
+									WHERE  
+										CASE -- Filtro 1
+											WHEN `Inv_Estatus` IS NOT NULL THEN `Inventario`.`Estatus`= `Inv_Estatus`
+											WHEN `Inv_Estatus` IS NULL THEN 1
+										END
+									AND 
+										CASE -- Filtro 2
+											WHEN `Inv_Folio` IS NOT NULL THEN `Inventario`.`Folio` = `Inv_Folio` 
+											WHEN `Inv_Folio` IS NULL THEN 1
+										END
+									AND
+										CASE -- Filtro 3
+											WHEN `Inv_Busqueda` IS NULL THEN 1
+											WHEN `Inv_Filtro` = 0	THEN CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`) LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 1	THEN `Inventario`.`Nombre_Prod`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 2	THEN `Inventario`.`Descripcion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 3	THEN `Inventario`.`Ubicacion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 4	THEN `Inventario`.`Marca`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 5	THEN `Inventario`.`Observaciones`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 6	THEN `Inventario`.`No_Serie`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 7	THEN `Inventario`.`Modelo`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 8	THEN `Inventario`.`Color`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 9	THEN `Inventario`.`Fecha_Compra`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
+											WHEN `Inv_Filtro` = 10	THEN `Inventario`.`Factura`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+										END;
+				END;
+			ELSE 
+				BEGIN
+					SELECT	CONCAT( `Inventario`.`Folio`, '-', 	`Inventario`.`Numero`, `Inventario`.`Extension`) AS 'Clave',
+									`Inventario`.`Nombre_Prod`,
+									`Inventario`.`Descripcion`,
+									`Inventario`.`Ubicacion`,
+									`Inventario`.`Marca`,
+									`Inventario`.`Observaciones`,
+									`Inventario`.`No_Serie`,
+									`Inventario`.`Modelo`,
+									`Inventario`.`Color`,
+									`Inventario`.`Fecha_Compra`,
+									`Inventario`.`Factura`,
+									`Inventario`.`Importe`,
+									CONCAT(	`Empleados`.`Nombres`, ' ', `Empleados`.`Apellido_P`, ' ', `Empleados`.`Apellido_M`) AS 'Responsable'  
+										FROM `INE`.`Vales`
+											INNER JOIN `INE`.`Detalle_Vale`		ON (`Detalle_Vale`.`ID_Vale`		= CONCAT(`Vales`.`Folio`, '-', `Vales`.`Numero`, '-', `Vales`.`Año`))
+											INNER JOIN `INE`.`Inventario`		ON (`Detalle_Vale`.`ID_Producto`	= CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`))
+											INNER JOIN `INE`.`Empleados`		ON (`Empleados`.`ID_Empleado`		= `Vales`.`ID_Empleado`)
+											WHERE  
+													CASE -- Filtro 1
+														WHEN `Inv_Estatus` IS NOT NULL THEN `Inventario`.`Estatus`= `Inv_Estatus`
+														WHEN `Inv_Estatus` IS NULL THEN 1
+													END
+												AND 
+													CASE -- Filtro 2
+														WHEN `Inv_Folio` IS NOT NULL THEN `Inventario`.`Folio` = `Inv_Folio` 
+														WHEN `Inv_Folio` IS NULL THEN 1
+													END
+												AND
+													CASE -- Filtro 3
+														WHEN `Inv_Busqueda` IS NULL THEN 1
+														WHEN `Inv_Filtro` = 0	THEN CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`) LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 1	THEN `Inventario`.`Nombre_Prod`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 2	THEN `Inventario`.`Descripcion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 3	THEN `Inventario`.`Ubicacion`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 4	THEN `Inventario`.`Marca`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 5	THEN `Inventario`.`Observaciones`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 6	THEN `Inventario`.`No_Serie`		LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 7	THEN `Inventario`.`Modelo`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 8	THEN `Inventario`.`Color`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 9	THEN `Inventario`.`Fecha_Compra`	LIKE CONCAT('%', `Inv_Busqueda`, '%')
+														WHEN `Inv_Filtro` = 10	THEN `Inventario`.`Factura`			LIKE CONCAT('%', `Inv_Busqueda`, '%')
+													END;
+				END;
+			END IF;
 		END;
 	ELSE 
-		BEGIN -- Esta parte ya funciona
+		BEGIN 
 			SELECT	CONCAT( `Inventario`.`Folio`, '-', 	`Inventario`.`Numero`, `Inventario`.`Extension`) AS 'Clave',
 				`Inventario`.`Nombre_Prod`,
 				`Inventario`.`Descripcion`,
@@ -216,21 +265,21 @@ BEGIN
 				`Inventario`.`Fecha_Compra`,
 				`Inventario`.`Factura`,
 				`Inventario`.`Importe`,
-				CONCAT(	`Empleados`.`Nombre`, ' ', `Empleados`.`Apellido_P`, ' ', `Empleados`.`Apellido_M`) AS 'Responsable'  
+				CONCAT(	`Empleados`.`Nombres`, ' ', `Empleados`.`Apellido_P`, ' ', `Empleados`.`Apellido_M`) AS 'Responsable'  
 					FROM `INE`.`Vales`
-						INNER JOIN `INE`.`Detalle_Vale`		ON (`Detalle_Vale`.`ID_Vale`		= CONCAT(`Vales`.`Folio`, '-', `Vales`.`Numero`, '-', `Vales`.`Año`))  -- `Vales`.`Año` ???
+						INNER JOIN `INE`.`Detalle_Vale`		ON (`Detalle_Vale`.`ID_Vale`		= CONCAT(`Vales`.`Folio`, '-', `Vales`.`Numero`, '-', `Vales`.`Año`))
 						INNER JOIN `INE`.`Inventario`		ON (`Detalle_Vale`.`ID_Producto`	= CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`))
 						INNER JOIN `INE`.`Empleados`		ON (`Empleados`.`ID_Empleado`		= `Vales`.`ID_Empleado`)
 							WHERE `Inventario`.`Estatus`= `Inv_Estatus`
 							AND 
 								CASE
-									-- Busqueda sin folio
-									WHEN `Inv_Busqueda` IS NOT NULL AND `Inv_Folio` IS NULL THEN 
-										CONCAT(`Empleados`.`Nombre`,' ',`Empleados`.`Apellido_P`,' ',`Empleados`.`Apellido_M`)	LIKE CONCAT('%', `Inv_Busqueda`, '%')
-									-- Busqueda con folio
-									WHEN `Inv_Busqueda` IS NOT NULL AND `Inv_Folio` IS NOT NULL THEN 
-										CONCAT(`Empleados`.`Nombre`,' ',`Empleados`.`Apellido_P`,' ',`Empleados`.`Apellido_M`)		LIKE CONCAT('%', `Inv_Busqueda`, '%') 
-										AND CONCAT( `Inventario`.`Folio`, '-', 	`Inventario`.`Numero`, `Inventario`.`Extension`)	LIKE CONCAT('%', `Inv_Folio`, '-', '%')
+									 WHEN `Inv_Busqueda` IS NULL THEN 1
+                                     WHEN `Inv_Busqueda` IS NOT NULL THEN CONCAT(`Empleados`.`Nombres`,' ',`Empleados`.`Apellido_P`,' ',`Empleados`.`Apellido_M`)	LIKE CONCAT('%', `Inv_Busqueda`, '%')
+								END
+                            AND
+								CASE
+									WHEN `Inv_Folio` IS NULL THEN 1
+									WHEN `Inv_Folio` IS NOT NULL THEN CONCAT( `Inventario`.`Folio`, '-', 	`Inventario`.`Numero`, `Inventario`.`Extension`)	LIKE CONCAT('%', `Inv_Folio`, '%')
 								END;
 		END;
 	END IF;
