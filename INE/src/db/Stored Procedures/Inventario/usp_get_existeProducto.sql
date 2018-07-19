9db3198d0054bba053ef9c39197b92b74dfa7c42
DeusArcana <DeusArcana@outlook.com>
2018-07-19 00:26:53 -0600

    Docs: existeProducto
    Documentación del SP usp_get_existeProducto

--- INE/src/db/Stored Procedures/Inventario/usp_get_existeProducto.sql
+++ INE/src/db/Stored Procedures/Inventario/usp_get_existeProducto.sql
@@ -1,8 +1,20 @@
+-- Borrado del SP
 DROP PROCEDURE IF EXISTS `usp_get_existeProducto`;
+
+-- ====================================================================================
+-- Author:      Javier Pazos
+-- Description: Revisa si existe un registro en la tabla de inventario
+--
+-- Parameters:
+--   @ID_Producto - Identificador del producto dado por su Folio, Numero y Extensión
+-- 
+-- Returns:     1	- Si existe un producto con el identificador dado
+-- 				2 	- Si NO existe un producto con el identificador dado
+-- ====================================================================================
 DELIMITER $$
 CREATE PROCEDURE `usp_get_existeProducto`(IN `ID_Producto` VARCHAR(255))
 BEGIN
-
+	-- Utilización de un IF - SELECT para la realización de la consulta
 	IF (SELECT 1 = 1 
 			FROM `INE`.`Inventario` 
 				WHERE CONCAT(`Inventario`.`Folio`, '-', `Inventario`.`Numero`, `Inventario`.`Extension`) = `ID_Producto`) THEN
