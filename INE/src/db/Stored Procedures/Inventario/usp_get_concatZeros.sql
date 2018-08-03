3e814a4dd9d603d5a3fdf2ac9b2d02a922d83ce0
DeusArcana <DeusArcana@outlook.com>
2018-08-03 00:26:31 -0600

    Docs: usp_get_concatZeros
    Se agregó la documentación correspondiente al SP, además de cambiar la
    validación del campo, donde retorna el valor solamente si existe el campo
    y si el valor contenido dentro de este campo no es NULL. Dado caso que no
    se cumpla alguna de estas condiciones el valor retornado es cero.

--- INE/src/db/Stored Procedures/Inventario/usp_get_concatZeros.sql
+++ INE/src/db/Stored Procedures/Inventario/usp_get_concatZeros.sql
@@ -1,14 +1,28 @@
 -- Borrado del SP
 DROP PROCEDURE IF EXISTS `usp_get_concatZeros`;
-
+-- ====================================================================
+-- Author:      Javier Pazos
+-- Description: Obtiene el número de dígitos de se deben contener en la 
+--				presentación de los ID de producto, este número está 
+-- 				previamente guardado en la base de datos.
+--
+-- Returns:     El número contenido en el campo `INT_String_Format` con
+--				la etiqueta 'res'.
+--				Cero en caso contrario
+-- ====================================================================
 DELIMITER $$
 CREATE PROCEDURE `usp_get_concatZeros`()
 BEGIN
-
-	IF EXISTS (SELECT 1 = 1 FROM `INE`.`Admin_Config` WHERE 1)
+	-- Comprueba que existe dicho valor
+	IF EXISTS	(SELECT `INT_String_Format` FROM `INE`.`Admin_Config` WHERE 1) 
+		AND 	(SELECT `INT_String_Format` FROM `INE`.`Admin_Config` WHERE 1) IS NOT NULL
 		THEN 
-			SELECT `INT_String_Format` AS 'res' FROM `INE`.`Admin_Config`;
+			-- Selecciona el campo de la tabla
+			SELECT `INT_String_Format` AS 'res' 
+				FROM `INE`.`Admin_Config`
+					WHERE 1 LIMIT 1;
 	ELSE 
+		-- Se retorna 0 como respuesta
 		SELECT 0 AS 'res';
 	END IF;
 
