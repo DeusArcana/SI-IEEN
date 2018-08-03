ddb596db004be7c636f81a2a4bd18e969dbc457f
DeusArcana <DeusArcana@outlook.com>
2018-08-03 00:33:16 -0600

    Docs: usp_get_infoFolio
    Documentación del SP

--- INE/src/db/Stored Procedures/Inventario/usp_get_infoFolio.sql
+++ INE/src/db/Stored Procedures/Inventario/usp_get_infoFolio.sql
@@ -1,8 +1,15 @@
+-- Borrado del SP
 DROP PROCEDURE IF EXISTS `usp_get_infoFolio`;
+-- ====================================================================
+-- Author:      Javier Pazos
+-- Description: Obtiene los registros contenidos en la tabla `Folio`
+--
+-- Returns:     ID de los Folios y su Descripción
+-- ====================================================================
 DELIMITER $$
 CREATE PROCEDURE `usp_get_infoFolio`()
 BEGIN
-
+	-- Consulta SELECT
 	SELECT `Folio`.`ID_Folio`, `Folio`.`Descripcion`
 		FROM `INE`.`Folio`
 			WHERE 1;
