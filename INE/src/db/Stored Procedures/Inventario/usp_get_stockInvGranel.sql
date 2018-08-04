94eb9a93879794468a812db941fc49c683879f27
DeusArcana <DeusArcana@outlook.com>
2018-08-03 22:09:52 -0600

    SP - Corrección: stockInvGranel
    La corrección consiste en retorna (- 1) si el registro no se encuentra en
    la tabla.

--- INE/src/db/Stored Procedures/Inventario/usp_get_stockInvGranel.sql
+++ INE/src/db/Stored Procedures/Inventario/usp_get_stockInvGranel.sql
@@ -3,9 +3,19 @@ DELIMITER $$
 CREATE PROCEDURE `usp_get_stockInvGranel`(IN `ID_Producto` VARCHAR(255))
 BEGIN
 
-	SELECT `Inventario_Granel`.`Stock`
-		FROM `INE`.`Inventario_Granel`
-			WHERE CONCAT(`Inventario_Granel`.`Folio`, '-', `Inventario_Granel`.`Numero`, `Inventario_Granel`.`Extension`) = `ID_Producto`;
+	IF (SELECT 1 = 1 
+			FROM `INE`.`Inventario_Granel` 
+				WHERE CONCAT(`Inventario_Granel`.`Folio`, '-', `Inventario_Granel`.`Numero`, `Inventario_Granel`.`Extension`) = `ID_Producto`) THEN
+		BEGIN
+			SELECT `Inventario_Granel`.`Stock` AS 'res'
+				FROM `INE`.`Inventario_Granel`
+					WHERE CONCAT(`Inventario_Granel`.`Folio`, '-', `Inventario_Granel`.`Numero`, `Inventario_Granel`.`Extension`) = `ID_Producto`;
+		END;
+	ELSE 
+		BEGIN
+			SELECT - 1 AS 'res';
+		END;
+	END IF;
 
 END$$
 DELIMITER ;
